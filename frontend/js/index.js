14e08b6960d1744ab1ced24a457fa2a26d579a13
henriquecondela-dev <hguambejunior@gmail.com>
2026-06-19 13:03:25 +0200

    modal aplicado com sucesso

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,8 +1,20 @@
 const leftSide=document.querySelector(".left-side");
 const button=document.getElementById("state-btn");
+const creatgroupBtn=document.getElementById("creatGrup-btn");
+const cancelBtn=document.getElementById("cancel-btn");
+const overlyG=document.getElementById("overlay");
+const modalG=document.getElementById("modal");
 
 button.addEventListener("click", function(){
   
     leftSide.classList.toggle("collapsed");
     //console.log(leftSide.className);
 });
+creatgroupBtn.addEventListener("click", function(){
+    overlyG.style.display="flex";
+    modalG.style.display="block";
+});
+cancelBtn.addEventListener("click", function(){
+     overlyG.style.display="none";
+    modalG.style.display="none";
+})
