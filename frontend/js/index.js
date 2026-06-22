2fd554211a12720109c3c260470fcf3792e4045a
henriquecondela-dev <hguambejunior@gmail.com>
2026-06-22 23:45:30 +0200

    Login and signup pages updated

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -5,7 +5,7 @@ const button=document.getElementById("state-btn");
 const creatgroupBtn=document.getElementById("creatGrup-btn");
 const cancelG=document.getElementById("creat-cancel-btn");
 const overlayG=document.getElementById("creat-overlay");
-const modalG=document.getElementById("creat-modal");
+
 
 //Join a groput consts
 const joinButton =document.getElementById("joinGroup-btn");
@@ -14,6 +14,26 @@ const groupsContainer =document.getElementById("groups-container");
 const cancelJ =document.getElementById("join-cancel-btn");
 const joinSelectedButton=document.getElementById("join-selected-btn");
 let selectedGroup = null;
+
+//start a challenge consts
+const startChallengebutton=document.getElementById("startGroup-btn")
+const overlayS=document.getElementById("start-overlay");
+const startSelectedButton=document.getElementById("start-selected-btn");
+const cancelS=document.getElementById("start-cancel-btn");
+const ownerGroupsContainer=document.getElementById("owner-groups-container");
+const startModal= document.getElementById("start-modal");
+let ownerSelectedGroup=null;
+const ownerGroups = [
+    {
+        id:8,
+        name:"Java Beginners"
+    },
+    {
+        id:13,
+        name:"Python Masters"
+    }
+];
+
 const groups = [
     {
         id:1,
@@ -57,6 +77,12 @@ joinButton.addEventListener("click", function(){
 cancelJ.addEventListener("click", function(){
      overlayJ.style.display="none";
 });
+startChallengebutton.addEventListener("click",function(){
+    overlayS.style.display="flex";
+});
+cancelS.addEventListener("click", ()=>{
+    overlayS.style.display="none";
+});
 
 function loadGroups(){
     if(groups.length===0){
@@ -67,9 +93,12 @@ function loadGroups(){
             const card=document.createElement("div");
             card.classList.add("group-card");
             card.textContent=group.name;
+            card.style.fontSize="14px";
+            card.style.fontWeight="bold";
             card.addEventListener("click", function(){
                 document.querySelectorAll(".group-card").forEach((element)=>{
                     element.classList.remove("selected-group");
+
                     //console.log(element);
                 });
                 card.classList.add("selected-group");
@@ -79,12 +108,51 @@ function loadGroups(){
             groupsContainer.appendChild(card);
         });
     }
+};
+function loadOwnerGroups(){
+    if(ownerGroups.length===0){
+        ownerGroupsContainer.innerHTML="<p>You have no groups created yet.</p>";
+        ownerGroupsContainer.style.textAlign="center";
+    }else{
+        ownerGroups.forEach((ownerGroup)=>{
+            const card=document.createElement("div");
+            card.classList.add("owner-group-card");
+            card.textContent=ownerGroup.name;
+            card.style.fontSize="14px";
+            card.style.fontWeight="bold";
+            card.addEventListener("click", function(){
+                document.querySelectorAll(".owner-group-card").forEach((element)=>{
+                    element.classList.remove("owner-selected-group");
+
+                    //console.log(element);
+                });
+                card.classList.add("owner-selected-group");
+                //console.log(selectedGroup);
+                ownerSelectedGroup=ownerGroup.id;
+            });
+           ownerGroupsContainer.appendChild(card);
+        });
+    }
 }
+
+/*startModal.addEventListener("click",()=>{
+    ownerSelectedGroup=null;
+});
+*/
+startSelectedButton.addEventListener("click",()=>{
+    if(ownerSelectedGroup===null){
+        showMessage("Plese select a group first","success","startMessage");
+    }else{
+        console.log("Starting a challenge", ownerSelectedGroup);
+    }
+});
 joinSelectedButton.addEventListener("click", function(){
     if (selectedGroup===null){
-        alert("please select a gropup");
+        //alert("please select a gropup");
+        showMessage("Plese select a group first","alert","joinMessage");
     }else{
         console.log("joining group", selectedGroup);
     }
 });
 loadGroups();
+loadOwnerGroups();
