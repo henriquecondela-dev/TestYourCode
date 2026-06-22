8ab7d70a3a2f84ff9b0ef1961548e77559040997
henriquecondela-dev <hguambejunior@gmail.com>
2026-06-22 18:37:32 +0200

    Pagina Principal melhorada, adicionei o modal para o botao join a group

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,9 +1,42 @@
+//left side consts
 const leftSide=document.querySelector(".left-side");
 const button=document.getElementById("state-btn");
+//creat group consts
 const creatgroupBtn=document.getElementById("creatGrup-btn");
-const cancelBtn=document.getElementById("cancel-btn");
-const overlyG=document.getElementById("overlay");
-const modalG=document.getElementById("modal");
+const cancelG=document.getElementById("creat-cancel-btn");
+const overlayG=document.getElementById("creat-overlay");
+const modalG=document.getElementById("creat-modal");
+
+//Join a groput consts
+const joinButton =document.getElementById("joinGroup-btn");
+const overlayJ =document.getElementById("join-overlay");
+const groupsContainer =document.getElementById("groups-container");
+const cancelJ =document.getElementById("join-cancel-btn");
+const joinSelectedButton=document.getElementById("join-selected-btn");
+let selectedGroup = null;
+const groups = [
+    {
+        id:1,
+        name:"Java Beginners"
+    },
+    {
+        id:2,
+        name:"Python Masters"
+    },
+    {
+        id:3,
+        name:"C++ Warriors"
+    },
+    {
+        id:4,
+        name:"Java Beginners"
+    },
+    {
+        id:5,
+        name:"Python Masters"
+    },
+   
+];
 
 button.addEventListener("click", function(){
   
@@ -11,10 +44,47 @@ button.addEventListener("click", function(){
     //console.log(leftSide.className);
 });
 creatgroupBtn.addEventListener("click", function(){
-    overlyG.style.display="flex";
-    modalG.style.display="block";
+    overlayG.style.display="flex";
+   
 });
-cancelBtn.addEventListener("click", function(){
-     overlyG.style.display="none";
-    modalG.style.display="none";
-})
+cancelG.addEventListener("click", function(){
+     overlayG.style.display="none";
+});
+joinButton.addEventListener("click", function(){
+    overlayJ.style.display="flex";
+   
+});
+cancelJ.addEventListener("click", function(){
+     overlayJ.style.display="none";
+});
+
+function loadGroups(){
+    if(groups.length===0){
+        groupsContainer.innerHTML="<p>No groups availible yet.</p>";
+        groupsContainer.style.textAlign="center";
+    }else{
+        groups.forEach((group)=>{
+            const card=document.createElement("div");
+            card.classList.add("group-card");
+            card.textContent=group.name;
+            card.addEventListener("click", function(){
+                document.querySelectorAll(".group-card").forEach((element)=>{
+                    element.classList.remove("selected-group");
+                    //console.log(element);
+                });
+                card.classList.add("selected-group");
+                //console.log(selectedGroup);
+                selectedGroup=group.id;
+            });
+            groupsContainer.appendChild(card);
+        });
+    }
+}
+joinSelectedButton.addEventListener("click", function(){
+    if (selectedGroup===null){
+        alert("please select a gropup");
+    }else{
+        console.log("joining group", selectedGroup);
+    }
+});
+loadGroups();
---
+++ frontend/js/terminal.js
@@ -0,0 +1,44 @@
+const terminal=document.getElementById("text");
+const terminalBox = document.getElementById("terminal");
+const texts=["",
+    "    import java.util.Scanner;",
+    "    public static void main(String[] args) {",
+    "        int[] armazem=new int[10];",
+    "        Scanner input=new Scanner(System.in);",
+    "        System.out.println('Digite os valores;');",
+    "        for(int i=0; i<armazem.length; i++) {",
+    "           System.out.print('numero '+i+1+' :');",
+    "           armazem[i]=input.nextInt();",
+    "        }",
+    "        int soma=0;",
+    "        for(int n:armazem){",
+    "            soma+=n;",
+    "        }",
+    "        System.out.println('Soma = '+soma);",
+    "    }",
+    "}"];
+let actualrow=0;
+let indice=0;
+
+function write(){
+    const currentText= texts[actualrow];
+    if(indice<currentText.length){
+          terminal.textContent +=currentText[indice];
+          terminalBox.scrollTop = terminalBox.scrollHeight;
+        indice++;
+        setTimeout(function(){
+            write();
+        }, 100);
+    }else{
+        terminal.textContent+="\n";
+        actualrow++;
+        indice=0;
+        if(actualrow<texts.length){
+            setTimeout(function(){
+                write()
+            },300);
+        }
+    }
+}
+write();
+
